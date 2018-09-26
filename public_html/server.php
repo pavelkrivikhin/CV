f3a00b480564838cad8d03b1c6d4f230f15af4c2
Pavel Krivikhin <professor.faust.st.p@gmail.com>
2018-09-26 21:26:17 +0300

    Запрограммировали серверный обработчик формы.

--- public_html/server.php
+++ public_html/server.php
@@ -1,3 +1,9 @@
 <?php
 
-var_dump($_POST);
+//получили данные из формы
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+//вызвали функцию отправки почты и передали ей аргументы
+mail('professor.faust.st.p@gmail.com', 'Сообщение с сайта', "От: $name, email: $email, текст сообщения: $message");

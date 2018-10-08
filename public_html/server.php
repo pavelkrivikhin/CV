3216435feac4eb3671832384b27aeabf7a6b6d28
Pavel Krivikhin <professor.faust.st.p@gmail.com>
2018-10-08 21:21:30 +0300

    Реализовали отправку формы на AJAX.

--- public_html/server.php
+++ public_html/server.php
@@ -8,9 +8,5 @@ $message = $_POST['message'];
 //вызвали функцию отправки почты, передали ей аргументы, сохранили в переменную результат вызова.
 $result = mail('professor.faust.st.p@gmail.com', 'Сообщение с сайта', "От: $name, email: $email, текст сообщения: $message");
 
-//проверяем результат работы функции и выводим соответствуещее сообщение.
-if($result){
-    echo 'Success! Your message was be send. <a href="/">Back to main page</a>';
-} else {
-    echo 'Error. Something went wrong. <a href="/">Back to main page</a>';
-}
+//проверяем результат работы функции и выводим соответствуещее сообщение в формате JSON.
+echo json_encode(['result' => $result]);
